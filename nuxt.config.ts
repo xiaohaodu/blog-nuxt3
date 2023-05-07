2b82936cc267e50d739d12b41f2dfaa1b6142f82
杜晓浩 <1923911906@qq.com>
2023-05-07 09:32:36 +0800

    添加百度快速收录

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,8 @@ export default defineNuxtConfig({
             },
             meta: [
                 { charset: 'utf-8' },
-                { name: 'referrer', content: 'no-referrer' }
+                { name: 'referrer', content: 'no-referrer' },
+                { name: "baidu-site-verification", content: "codeva-RaDFb6xjmj" }//百度快速收录
             ],
             link: [
                 { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },

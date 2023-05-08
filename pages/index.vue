5a884d47d2ab605c3d6dd3c917172d24e45a9af6
杜晓浩 <1923911906@qq.com>
2023-05-08 13:56:35 +0800

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h2>
-            <div>这里什么都没有，去看看 <nuxt-link to="/blogs/README">博客</nuxt-link>吧!</div>
+            <div>这里什么都没有，去看看 <nuxt-link to="/blogs/README">博客</nuxt-link>吧!吧!吧!吧!吧!</div>
         </h2>
     </div>
 </template>

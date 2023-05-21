e9d297014b7b19440fb97ea98e1ec6b79fc0678a
杜晓浩 <1923911906@qq.com>
2023-05-21 21:55:46 +0800

    debug：修正router配置

--- app/router.options.ts
+++ app/router.options.ts
@@ -3,6 +3,7 @@ import type { RouterConfig } from '@nuxt/schema';
 // https://router.vuejs.org/api/interfaces/routeroptions.html
 export default <RouterConfig>{
     routes: (_routes) => [
+        ..._routes,
         {
             path: '/blogs/:blogPath+',
             component: () => import('~/pages/blogs/[blogPath].vue')

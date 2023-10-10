74eec47c706351502fc9e0ef29697f6d859dce65
杜晓浩 <1923911906@qq.com>
2023-10-10 16:10:29 +0800

    feat: docker 配置

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-        <h2>
-            <div>这里什么都没有，去看看 <nuxt-link to="/blogs/README">博客</nuxt-link>吧!</div>
-        </h2>
-    </div>
+  <div>
+    <h2>
+      <div>
+        这里什么都没有，去看看 <nuxt-link to="/blogs/README">博客</nuxt-link>吧!
+      </div>
+    </h2>
+  </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style lang="scss" scoped></style>
--- test.ts
+++
@@ -1,8 +0,0 @@
-enum Gender {
-    Male,
-    Female,
-    Other
-}
-console.log(Gender.Female); // Output: 0
-//We can also access an enum value by it's number value.
-console.log(Gender[1]); // Output: Female

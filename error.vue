c208156d5a4e07ff0a874cafd67c3f719e0c03e4
杜晓浩 <1923911906@qq.com>
2023-10-10 16:35:54 +0800

    fix: 重新生成依赖lock文件

--- error.vue
+++ error.vue
@@ -1,34 +1,34 @@
 <template>
-    <div class="NotFound">
-        <img src="@/assets/imgs/NotFound.svg" />
-        <div class="tip">/404/<br><br>一只狗吃了这个页面</div>
-    </div>
+  <div class="NotFound">
+    <img src="@/assets/imgs/NotFound.svg" />
+    <div class="tip">/404/<br /><br />一只狗吃了这个页面</div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 const props = defineProps({
-    error: Object
+  error: Object,
 });
 </script>
 
 <style lang="scss" scoped>
 .NotFound {
-    height: 100vh;
-    width: 100vw;
-    background-color: rgb(255, 228, 234);
-    overflow: hidden;
+  height: 100vh;
+  width: 100vw;
+  background-color: rgb(255, 228, 234);
+  overflow: hidden;
 
-    >img {
-        height: 60vh;
-        display: block;
-        margin: 0 auto;
-    }
+  > img {
+    height: 60vh;
+    display: block;
+    margin: 0 auto;
+  }
 
-    .tip {
-        font-size: 42px;
-        font-weight: 600;
-        text-align: center;
-        margin-bottom: 15px;
-    }
+  .tip {
+    font-size: 42px;
+    font-weight: 600;
+    text-align: center;
+    margin-bottom: 15px;
+  }
 }
 </style>

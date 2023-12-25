8bc5fceba1621cf502a0f7aa577aff013fe92763
杜晓浩 <1923911906@qq.com>
2023-12-25 15:26:49 +0800

    fix: layouts/default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,22 @@
 <template>
   <Navigation></Navigation>
-  <slot name="main">
-    <router-view></router-view>
-  </slot>
-  <slot name="foot">
-    <div class="fixed bottom-2 w-full text-xs">
-      <div class="flex w-full items-center justify-center">
-        <a href="https://beian.miit.gov.cn/" target="_blank">冀ICP备2022022916</a>
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <a
-          href="https://beian.mps.gov.cn/#/query/webSearch?code=51159902000145"
-          rel="noreferrer"
-          target="_blank"
-          >川公网安备51159902000145</a
-        >
+  <div>
+    <slot name="main">
+      <router-view></router-view>
+    </slot>
+    <slot name="foot">
+      <div class="fixed bottom-2 w-full text-xs">
+        <div class="flex w-full items-center justify-center">
+          <a href="https://beian.miit.gov.cn/" target="_blank">冀ICP备2022022916</a>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <a
+            href="https://beian.mps.gov.cn/#/query/webSearch?code=51159902000145"
+            rel="noreferrer"
+            target="_blank"
+            >川公网安备51159902000145</a
+          >
+        </div>
       </div>
-    </div>
-  </slot>
+    </slot>
+  </div>
 </template>
